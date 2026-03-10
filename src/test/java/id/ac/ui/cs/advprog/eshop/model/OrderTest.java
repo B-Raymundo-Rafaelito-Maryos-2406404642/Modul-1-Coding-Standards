7bda83e3a7774b6674ef8ebbdb49ef2e04c341d1
Raymundo7532 <woloblocrafael@gmail.com>
2026-03-10 12:54:27 +0700

    [RED] Add Order model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/OrderTest.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.ArrayList;
